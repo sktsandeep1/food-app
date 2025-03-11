d36e34841841e240b2c2817a47afb57fb5fe49b2
sktsandeep1 <sktsandeep1@gmail.com>
2025-03-12 01:22:18 +0530

    changed mongo compass to mongo atlas

--- config/config.js
+++ config/config.js
@@ -1,17 +1,18 @@
-    const mongoose = require("mongoose");
-    require('colors')
+const mongoose = require("mongoose");
+require("colors");
 
-    //connect DB Funtion
-    const connectDb = async () => {
-        try {
-            const connectt = await mongoose.connect(process.env.MONGO_URI);
-            console.log(`mongodb connected ${connectt.connection.host}`.bgYellow);
-        } catch (error){
-            console.log(`Error :${error.message}`. bgRed);
-            process.exit(1);
-            
-        }
-    };
+//connect DB Funtion
+const connectDb = async () => {
+  try {
+    const connectt = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`mongodb connected ${connectt.connection.host}`.bgYellow);
+    useNewUrlParser: true;
+    useUnifiedTopology: true;
+  } catch (error) {
+    console.log(`Error :${error.message}`.bgRed);
+    process.exit(1);
+  }
+};
 
 // exports
-    module.exports = connectDb;
+module.exports = connectDb;
--- seeder.js
+++ seeder.js
@@ -1,25 +1,24 @@
-const mongoose = require ("mongoose")
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const connectDb = require ('./config/config');
-const itemModel = require('./models/item.model');
-const dataItems = require('./utils/data');
+const connectDb = require("./config/config");
+const itemModel = require("./models/item.model");
+const dataItems = require("./utils/data");
 
 // config
-dotenv.config()
+dotenv.config();
 connectDb();
-
 
 // function seeder
 
 const importData = async () => {
-    try {
-        await itemModel.deleteMany()
-        const itemsdata = await itemModel.insertMany(dataItems);
-        console.log('all items added'.bgGreen)
-        process.exit();
-    } catch (error) {
-        console.log(`${error}`.bgRed.inverse);
-        process.exit(1)
-    }
-}
- importData();
+  try {
+    await itemModel.deleteMany();
+    const itemsdata = await itemModel.insertMany(dataItems);
+    console.log("all items added".bgGreen);
+    process.exit();
+  } catch (error) {
+    console.log(`${error}`.bgRed.inverse);
+    process.exit(1);
+  }
+};
+importData();
